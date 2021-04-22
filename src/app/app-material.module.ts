298a1611fed9d081718ba275cb5d7f6bfc8b3c0d
Poleton <pol.fernandez.blanquez@estudiantat.upc.edu>
2021-04-22 15:44:06 +0200

    1 file modified

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,24 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {MatTableModule} from '@angular/material/table'
-import {MatTabsModule} from '@angular/material/tabs';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
 import { TableComponent } from './table/table.component'
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    MatTabsModule,
-    MatTableModule,
-    MatDividerModule,
-  ],
-  exports: [
-    CommonModule,
-    MatTabsModule,
-    MatTableModule,
-    MatDividerModule,
-  ]
-
+  imports: [CommonModule, MatTabsModule, MatTableModule, MatDividerModule],
+  exports: [CommonModule, MatTabsModule, MatTableModule, MatDividerModule]
 })
-export class AppMaterialModule { }
+export class AppMaterialModule {}
