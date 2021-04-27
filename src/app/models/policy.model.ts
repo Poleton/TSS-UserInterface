1c4988a0cc4671be0f3d7c0f75a2de7b4905a604
Poleton <pol.fernandez.blanquez@estudiantat.upc.edu>
2021-04-27 20:02:10 +0200

    6 files modified

--- src/app/models/policy.model.ts
+++ src/app/models/policy.model.ts
@@ -1,6 +1,11 @@
 export class Policy {
     id!: number;
     name!: string;
-    username!: string;
-    email!: string;
+    duration!: string;
+    territorialScope!: string; //countries where the delivery will take place 
+    meansOfTransportation!: string; 
+    numSensors!: number;
+    conditions!: number; //[dictionary ( values range –  % reserve per min )] sets the conditions in which the shipped goods will have to be compensated all time units in the conditions measurements are in minutes 
+
+
 }
--- src/app/policy-table/policy-table.component.ts
+++ src/app/policy-table/policy-table.component.ts
@@ -1,26 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { Policy } from '../models/policy.model';
-import { RestService } from '../services/rest.service';
-
+import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { Policy } from "../models/policy.model";
+import { RestService } from "../services/rest.service";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 @Component({
-  selector: 'app-policy-table',
-  templateUrl: './policy-table.component.html',
-  styleUrls: ['./policy-table.component.css']
+  selector: "app-policy-table",
+  templateUrl: "./policy-table.component.html",
+  styleUrls: ["./policy-table.component.css"]
 })
 export class PolicyTableComponent implements OnInit {
+  displayedColumns: string[] = ["id", "name", "date", "condition"];
+  dataSource: Policy[] = [];
 
-  displayedColumns: string[] = ['id', 'name', 'date', 'condition'];
-  dataSource : Policy[] = [];
-
-  constructor(private restService: RestService) {
+  constructor(private restService: RestService) {}
+  
+  /*Filtratge
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
-  ngOnInit()  {
-    this.restService.getPolicies()
-    .subscribe(rest => this.dataSource = rest);
-   
+  */
+  
+  ngOnInit() {
+    this.restService.getPolicies().subscribe(rest => (this.dataSource = rest));
   }
-
-
 }
