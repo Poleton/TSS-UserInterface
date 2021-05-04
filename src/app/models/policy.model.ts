bfd87422121b46e92c1ea81e92126996c18c167a
Poleton <pol.fernandez.blanquez@hotmail.com>
2021-05-04 20:03:57 +0200

    "FIX:{policy.model definitive}"

--- src/app/models/policy.model.ts
+++ src/app/models/policy.model.ts
@@ -1,8 +1,21 @@
 export class Policy {
-    id!: number;
+    //Angular->API
+    policyName!: string;
+    policyHolderCIF!: string;
+    policyHolderName!: string; 
     product!: string;
+    territorialScope!: string;
+    contractPremium!: number;
+    contractLiability!: number;
+    expiryTimestamp!: number;
+    //API->Angular
+    id!: number;
+    inceptionTimestamp!: number;
+    isSmart!: boolean; 
+    //Added for the customer
     description!: string;
     holderName!: string;
     mailAdress!: string;
     duration!: string;
+    
 }
